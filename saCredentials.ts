04add613ca7677e82914b79b484c7f7732745c8d
EckZar <32589564+EckZar@users.noreply.github.com>
2021-10-27 23:10:36 +0300

    Update saCredentials.ts

--- saCredentials.ts
+++ saCredentials.ts
@@ -1,13 +1,13 @@
 const saCredentials = {
     "type": "service_account",
-    "project_id": "script-api-330118",
-    "private_key_id": "58c1f0adf14752be7907550d91ed2ac8b0e68875",
+    "project_id": "",
+    "private_key_id": "",
     "private_key": "-----BEGIN PRIVATE KEY-----\n==\n-----END PRIVATE KEY-----\n",
     "client_email": "scripapi@script-api-330118.iam.gserviceaccount.com",
-    "client_id": "116307729008513525888",
+    "client_id": "",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/scripapi%40script-api-330118.iam.gserviceaccount.com"
+    "client_x509_cert_url": ""
   }
   
